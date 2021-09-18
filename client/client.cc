1421ec894e0362264295fa8a79f50d8f6d3296c6
TimSong412 <3190101087@zju.edu.cn>
2021-09-18 17:03:41 +0800

    update conn & disconn

--- client/client.cc
+++ client/client.cc
@@ -7,14 +7,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <iostream>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include "../MsgDef.hpp"
 
-#define SERVER_PORT	6666 //侦听端口
+using namespace std;
 
 extern int errno;
 
@@ -26,51 +28,84 @@ struct student
 };
 
 int main(int argc, char *argv[])
-{	
+{
 	int sockfd;
+	int option;
+	unsigned msg_type;
+	string ipaddr;
+	int port;
 	struct sockaddr_in serverAddr;
 	struct student stu;
-	
-	if(argc != 4)
-	{
-		printf("usage: informLinuxClient serverIP name age\n");
-		return -1;
-	}
 
-	// 创建一个socket：
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	while (1)
 	{
-		printf("socket() failed! code:%d\n", errno);
-		return -1;
-	}
-		
-	// 设置服务器的地址信息：
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(SERVER_PORT);
-	serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
-	bzero(&(serverAddr.sin_zero), 8);
-		
-	//客户端发出连接请求：
-	printf("connecting!\n");
-	if(connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
-	{
-		printf("connect() failed! code:%d\n", errno);
-		close(sockfd);
-		return -1;
-	}
+		cout << "please enter service number: \n-1 Connect Server\n-2 Close Connection\n-3 Get Time\n-4 Get Name\n-5 Get List\n-6 Send Message\n-7 Quit\n";
+		cin >> option;
 
-	printf("Connected!\n");
-	
-	strcpy(stu.name, argv[2]);
-	stu.age = atoi(argv[3]);
-	if(send(sockfd, (char *)&stu, sizeof(stu), 0) == -1)
-	{
-		printf("send() failed!\n");
-		close(sockfd);
-		return -1;
-	}
+		switch (option)
+		{
+		case 1:
+			cout << "please enter IP address:" << endl;
+			cin >> ipaddr;
+			cout << "please enter port number:" << endl;
+			cin >> port;
 
-	printf("student info has been sent!\n");
-	close(sockfd);//关闭套接字
-	return 0;
+			// 创建一个socket：
+			if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+			{
+				printf("socket() failed! code:%d\n", errno);
+				return -1;
+			}
+
+			// 设置服务器的地址信息：
+			serverAddr.sin_family = AF_INET;
+			serverAddr.sin_port = htons(port);
+			serverAddr.sin_addr.s_addr = inet_addr(ipaddr.c_str());
+			bzero(&(serverAddr.sin_zero), 8);
+
+			//客户端发出连接请求：
+			printf("Connecting to Server %s : %d\n", ipaddr.c_str(), port);
+			if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+			{
+				printf("connect() failed! code:%d\n", errno);
+				close(sockfd);
+				return -1;
+			}
+
+			printf("Connected!\n");
+			break;
+
+		case 2:
+			//关闭socket
+			msg_type = static_cast<unsigned>(MsgType::kDisconnect);
+			if (send(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0) == -1)
+				cout << "Communication Error, close locally." << endl;			
+			close(sockfd);
+			cout << "Connection to " << ipaddr << " : " << port << " is closed! \n"
+				 << endl;
+			break;
+
+		case 3:
+			//获取时间
+
+		case 7:
+			cout << "Bye bye~\n\n";
+			return 0;
+		default:
+			break;
+		}
+
+		// strcpy(stu.name, argv[2]);
+		// stu.age = atoi(argv[3]);
+		// if (send(sockfd, (char *)&stu, sizeof(stu), 0) == -1)
+		// {
+		// 	printf("send() failed!\n");
+		// 	close(sockfd);
+		// 	return -1;
+		// }
+
+		// printf("student info has been sent!\n");
+		// close(sockfd); //关闭套接字
+		// return 0;
+	}
 }
