4d8e8792b20890f838ad7fc03beeb8c4971a112f
TimSong412 <3190101087@zju.edu.cn>
2021-09-21 23:14:35 +0800

    client most function down

--- client/client.cc
+++ client/client.cc
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/stat.h>
@@ -19,7 +20,9 @@ using namespace std;
 
 extern int errno;
 int sockfd;
-int Request();
+bool conn;
+bool quit;
+int Request(int option);
 void RecvMsg();
 
 ThreadSafeQueue<string> msgQ;
@@ -27,14 +30,25 @@ ThreadSafeQueue<string> msgQ;
 int main(int argc, char *argv[])
 {
 	string msg;
-
+	int option;
+	quit = false;
+	conn = false;
 	future<void> recvMsg = async(launch::async, RecvMsg);
 
 	while (1)
 	{
 		// recvMsg.wait_for(chrono::milliseconds(1));
 		if (msgQ.empty())
-			Request();
+		{
+			cout << "please enter service number: \n-1 Connect Server\n-2 Close Connection\n-3 Get Time\n-4 Get HostName\n-5 Get List\n-6 Send Message\n-7 Quit\n";
+			cin >> option;
+			if (!(Request(option) == -1))
+				std::this_thread::sleep_for(10ms);
+			else
+			{
+				return 0;
+			}
+		}
 		else
 		{
 			msgQ.pop(msg);
@@ -46,39 +60,96 @@ int main(int argc, char *argv[])
 void RecvMsg()
 {
 	unsigned msg_type;
+	int count;
+	int fd;
 	MsgType option;
- 	size_t name_len;
+	size_t name_len;
 	string Hname;
-	recv(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(unsigned), 0);
-	option = static_cast<MsgType>(msg_type);
-	switch (option)
-	{
-	case MsgType::kHostname:
-	{
-		recv(sockfd, reinterpret_cast<void *>(&name_len), sizeof(name_len), 0);
-		Hname.resize(name_len);
-		recv(sockfd, reinterpret_cast<void *>(Hname.data()), sizeof(char) * name_len, 0);
+	time_t cur_time;
+	struct tm *timestr;
+	string time_str;
+	size_t list_len;
+	sockaddr_in addr;
+	char addr_str[INET_ADDRSTRLEN];
+	string num;
+	string temp;
+	string port;
+	size_t msg_len;
+	int src;
+	string msg_content;
 
-		msgQ.push("Your Hostname is: "+Hname);
-		break;
-	}
-	case MsgType::kTime:
+	while (1)
 	{
-		break;
-	}
 
-	default:
-		break;
+		ioctl(sockfd, FIONREAD, &count);
+		if (quit)
+			break;
+		if (conn && count)
+		{
+			recv(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(unsigned), 0);
+			option = static_cast<MsgType>(msg_type);
+			switch (option)
+			{
+			case MsgType::kHostname:
+			{
+				recv(sockfd, reinterpret_cast<void *>(&name_len), sizeof(name_len), 0);
+				Hname.resize(name_len);
+				recv(sockfd, reinterpret_cast<void *>(Hname.data()), sizeof(char) * name_len, 0);
+				msgQ.push("Your Hostname is: " + Hname + "\n");
+				break;
+			}
+			case MsgType::kTime:
+			{
+				recv(sockfd, reinterpret_cast<void *>(&cur_time), sizeof(cur_time), 0);
+				timestr = localtime(&cur_time);
+				time_str = asctime(timestr);
+				msgQ.push("Current Time is: " + time_str);
+
+				break;
+			}
+
+			case MsgType::kList:
+			{
+				recv(sockfd, reinterpret_cast<void *>(&list_len), sizeof(list_len), 0);
+				for (size_t i = 0; i < list_len; i++, cout << endl)
+				{
+					recv(sockfd, reinterpret_cast<void *>(&fd), sizeof(fd), 0);
+					recv(sockfd, reinterpret_cast<void *>(&addr), sizeof(addr), 0);
+					inet_ntop(AF_INET, &addr.sin_addr, addr_str, sizeof(addr_str));
+					num = to_string(fd);
+					port = to_string(addr.sin_port);
+					temp = "Socket fd: " + num + " Host Address: " + addr_str + ":" + port;
+					msgQ.push(temp);
+				}
+				msgQ.push("\n");
+			}
+
+			case MsgType::kMsg:
+			{
+				recv(sockfd, reinterpret_cast<void *>(&src), sizeof(src), 0);
+				recv(sockfd, reinterpret_cast<void *>(&msg_len), sizeof(msg_len), 0);
+				msg_content.resize(msg_len);
+				recv(sockfd, reinterpret_cast<void *>(msg_content.data()), sizeof(char) * msg_len, 0);
+				num = to_string(src);
+				temp = "Message from Host " + num + ": " + msg_content + "\n";
+				msgQ.push(temp);
+			}
+
+			default:
+				break;
+			}
+		}
 	}
 }
 
-int Request()
+int Request(int option)
 {
-	int option;
+	// int option;
 	unsigned msg_type;
 	string ipaddr;
 	static int port;
 	struct sockaddr_in serverAddr;
+	string temp;
 
 	// std::atomic<bool> heart_beating = true;
 	// std::future<void> alive = std::async(
@@ -93,9 +164,6 @@ int Request()
 	// 		}
 	// 	},
 	// 	&sockfd, &heart_beating);
-
-	cout << "please enter service number: \n-1 Connect Server\n-2 Close Connection\n-3 Get Time\n-4 Get Name\n-5 Get List\n-6 Send Message\n-7 Quit\n";
-	cin >> option;
 
 	switch (option)
 	{
@@ -129,6 +197,7 @@ int Request()
 			break;
 		}
 		cout << "Connect Success!" << endl;
+		conn = true;
 		break;
 	}
 	case 2:
@@ -140,37 +209,38 @@ int Request()
 		close(sockfd);
 		cout << "Connection to " << ipaddr << " : " << port << " is closed! \n"
 			 << endl;
+		conn = false;
 		break;
 	}
 	case 3:
 	{
 		//获取时间
-		time_t cur_time;
-		struct tm *timestr;
+		// time_t cur_time;
+		// struct tm *timestr;
 		msg_type = static_cast<unsigned>(MsgType::kTime);
 		if (send(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0) == -1)
 			cout << "Communication Error! " << endl;
 		else
 		{
-			recv(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0);
-			recv(sockfd, reinterpret_cast<void *>(&cur_time), sizeof(cur_time), 0);
-			const MsgType msg_type_ = static_cast<MsgType>(msg_type);
-			const time_t cur_time_ = static_cast<time_t>(cur_time);
-			if (msg_type_ != MsgType::kTime)
-				cout << "Recieve Type Error! Code: " << static_cast<int>(msg_type_) << endl;
-			else
-			{
-				timestr = localtime(&cur_time_);
-				string time_str = asctime(timestr);
-				cout << "Current Time is: " << time_str << endl;
-			}
+			// recv(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0);
+			// recv(sockfd, reinterpret_cast<void *>(&cur_time), sizeof(cur_time), 0);
+			// const MsgType msg_type_ = static_cast<MsgType>(msg_type);
+			// const time_t cur_time_ = static_cast<time_t>(cur_time);
+			// if (msg_type_ != MsgType::kTime)
+			// 	cout << "Recieve Type Error! Code: " << static_cast<int>(msg_type_) << endl;
+			// else
+			// {
+			// 	timestr = localtime(&cur_time_);
+			// 	string time_str = asctime(timestr);
+			// 	cout << "Current Time is: " << time_str << endl;
+			// }
 		}
 		break;
 	}
 	case 4:
 	{
-		string Hname;
-		size_t name_len;
+		// string Hname;
+		// size_t name_len;
 		msg_type = static_cast<unsigned>(MsgType::kHostname);
 		if (send(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0) == -1)
 			cout << "Communication Error! " << endl;
@@ -192,30 +262,30 @@ int Request()
 	}
 	case 5:
 	{
-		size_t list_len;
-		int fd;
-		sockaddr_in addr;
-		char addr_str[INET_ADDRSTRLEN];
+		// size_t list_len;
+		// int fd;
+		// sockaddr_in addr;
+		// char addr_str[INET_ADDRSTRLEN];
 		msg_type = static_cast<unsigned>(MsgType::kList);
 		if (send(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0) == -1)
 			cout << "Communication Error! " << endl;
 		else
 		{
-			recv(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0);
-			recv(sockfd, reinterpret_cast<void *>(&list_len), sizeof(list_len), 0);
-			const MsgType msg_type_ = static_cast<MsgType>(msg_type);
-			if (msg_type_ != MsgType::kList)
-				cout << "Recieve Type Error! Code: " << static_cast<int>(msg_type_) << endl;
-			else
-			{
-				for (size_t i = 0; i < list_len; i++, cout << endl)
-				{
-					recv(sockfd, reinterpret_cast<void *>(&fd), sizeof(fd), 0);
-					recv(sockfd, reinterpret_cast<void *>(&addr), sizeof(addr), 0);
-					inet_ntop(AF_INET, &addr.sin_addr, addr_str, sizeof(addr_str));
-					cout << "Socket fd " << fd << " Host Address " << addr_str << " : " << addr.sin_port;
-				}
-			}
+			// recv(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0);
+			// // recv(sockfd, reinterpret_cast<void *>(&list_len), sizeof(list_len), 0);
+			// const MsgType msg_type_ = static_cast<MsgType>(msg_type);
+			// if (msg_type_ != MsgType::kList)
+			// 	cout << "Recieve Type Error! Code: " << static_cast<int>(msg_type_) << endl;
+			// else
+			// {
+			// 	// for (size_t i = 0; i < list_len; i++, cout << endl)
+			// 	// {
+			// 	// 	recv(sockfd, reinterpret_cast<void *>(&fd), sizeof(fd), 0);
+			// 	// 	recv(sockfd, reinterpret_cast<void *>(&addr), sizeof(addr), 0);
+			// 	// 	inet_ntop(AF_INET, &addr.sin_addr, addr_str, sizeof(addr_str));
+			// 	// 	cout << "Socket fd " << fd << " Host Address " << addr_str << " : " << addr.sin_port;
+			// 	// }
+			// }
 		}
 		break;
 	}
@@ -232,13 +302,13 @@ int Request()
 		{
 			cout << "please enter your destination host:" << endl;
 			cin >> dst;
-			cout << "dst= " << dst << endl;
 			if (send(sockfd, reinterpret_cast<void *>(&dst), sizeof(dst), 0) == -1)
 				cout << "Communication Error! " << endl;
 			else
 			{
 				cout << "please enter your message to host " << dst << " :" << endl;
-				cin >> msg;
+				getline(cin, msg);
+				getline(cin, msg);
 				msg_len = msg.length() * sizeof(char);
 				if (send(sockfd, reinterpret_cast<void *>(&msg_len), sizeof(msg_len), 0) == -1)
 					cout << "Communication Error! " << endl;
@@ -249,11 +319,10 @@ int Request()
 					recv(sockfd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type), 0);
 					const MsgType msg_type_ = static_cast<MsgType>(msg_type);
 					if (msg_type_ == MsgType::kSuccess)
-						cout << "Message Sent Success!\n"
-							 << endl;
+						cout << "Message Sent Success!" << endl;
 					else
-						cout << "Message Sent Error!\n"
-							 << endl;
+						cout << "Message Sent Error!" << endl;
+					cin.sync();
 				}
 			}
 		}
@@ -261,12 +330,16 @@ int Request()
 	}
 
 	case 7:
+	{
 		// heart_beating = false;
 		cout << "Bye bye~" << endl;
-		return 0;
+		quit = true;
+		return -1;
+	}
 
 	default:
 		cout << "Wrong Option Number!" << endl;
+		getline(cin, temp);
 		break;
 	}
 	return 0;
--- server/Pool.cc
+++ server/Pool.cc
@@ -13,47 +13,60 @@
 extern int errno;
 
 template <typename... Args>
-void SendMessage(Args &&...args) {
-  if (!~send(std::forward<Args>(args)...)) {
+void SendMessage(Args &&...args)
+{
+  if (!~send(std::forward<Args>(args)...))
+  {
     std::cout << "send() failed! errno: " << errno << std::endl;
   }
 }
 
 template <typename... Args>
-void RecvMessage(Args &&...args) {
-  if (!~recv(std::forward<Args>(args)...)) {
+void RecvMessage(Args &&...args)
+{
+  if (!~recv(std::forward<Args>(args)...))
+  {
     std::cout << "recv() failed! errno: " << errno << std::endl;
   }
 }
 
-Pool::Pool(const std::string &hostname) {
+Pool::Pool(const std::string &hostname)
+{
   is_exit_ = false;
   hostname_ = hostname;
   hostname_len_ = hostname_.length();
 }
 
-Pool::~Pool() {
+Pool::~Pool()
+{
   is_exit_ = true;
-  for (const auto &thread : threads_) {
-    if (thread.second->joinable()) thread.second->join();
+  for (const auto &thread : threads_)
+  {
+    if (thread.second->joinable())
+      thread.second->join();
   }
 }
 
-void Pool::AddClient(const sockaddr_in &addr, const socket_fd &fd) {
-  if (is_exit_) return;
+void Pool::AddClient(const sockaddr_in &addr, const socket_fd &fd)
+{
+  if (is_exit_)
+    return;
   {
     std::lock_guard<std::mutex> lock(clients_mutex_);
     clients_[fd] = addr;
   }
-  auto f = [this, addr, fd]() {
+  auto f = [this, addr, fd]()
+  {
     std::mutex mutex;
     std::atomic<bool> recv_heartbeat = false;
     std::future<void> alive = std::async(
         std::launch::async,
-        [fd](std::mutex *const mutex, std::atomic<bool> *recv_heartbeat) {
+        [fd](std::mutex *const mutex, std::atomic<bool> *recv_heartbeat)
+        {
           int heartbeat_counter = 0;
           unsigned msg_type = static_cast<unsigned>(MsgType::kHeartBeat);
-          for (;;) {
+          for (;;)
+          {
             std::this_thread::sleep_for(kHeartBeatInterval);
             // { // TODO: uncomment this block
             //   std::lock_guard<std::mutex> lock(*mutex);
@@ -64,20 +77,26 @@ void Pool::AddClient(const sockaddr_in &addr, const socket_fd &fd) {
             //               << std::endl;
             //   }
             // }
-            if (*recv_heartbeat) {
+            if (*recv_heartbeat)
+            {
               heartbeat_counter = 0;
               *recv_heartbeat = false;
-            } else {
+            }
+            else
+            {
               heartbeat_counter++;
-              if (heartbeat_counter >= kHeartBeatThreshold) return;
+              if (heartbeat_counter >= kHeartBeatThreshold)
+                return;
             }
           }
         },
         &mutex, &recv_heartbeat);
     unsigned msg_type;
-    for (;;) {
+    for (;;)
+    {
       // exit
-      if (is_exit_) {
+      if (is_exit_)
+      {
         msg_type = static_cast<unsigned>(MsgType::kDisconnect);
         {
           std::lock_guard<std::mutex> lock(mutex);
@@ -88,7 +107,8 @@ void Pool::AddClient(const sockaddr_in &addr, const socket_fd &fd) {
         return;
       }
       // forward message
-      if (!msg_queues_[fd].empty()) {
+      if (!msg_queues_[fd].empty())
+      {
         msg_type = static_cast<unsigned>(MsgType::kMsg);
         MessageInfo msg;
         msg_queues_[fd].pop(msg);
@@ -110,105 +130,122 @@ void Pool::AddClient(const sockaddr_in &addr, const socket_fd &fd) {
       // handle requestint count;
       int count;
       ioctl(fd, FIONREAD, &count);
-      if (count) {
+      if (count)
+      {
         RecvMessage(fd, reinterpret_cast<void *>(&msg_type), sizeof(msg_type),
                     0);
         const MsgType msg_type_ = static_cast<MsgType>(msg_type);
-        switch (msg_type_) {
-          case MsgType::kTime: {
-            std::time_t cur_time = time(nullptr);
-            {
-              std::lock_guard<std::mutex> lock(mutex);
-              SendMessage(fd, reinterpret_cast<void *>(&msg_type),
-                          sizeof(msg_type), 0);
-              SendMessage(fd, reinterpret_cast<void *>(&cur_time),
-                          sizeof(cur_time), 0);
-            }
-          } break;
-          case MsgType::kHostname: {
+        switch (msg_type_)
+        {
+        case MsgType::kTime:
+        {
+          std::time_t cur_time = time(nullptr);
+          {
             std::lock_guard<std::mutex> lock(mutex);
             SendMessage(fd, reinterpret_cast<void *>(&msg_type),
                         sizeof(msg_type), 0);
-            SendMessage(fd, reinterpret_cast<void *>(&hostname_len_),
-                        sizeof(hostname_len_), 0);
-            SendMessage(fd, reinterpret_cast<void *>(hostname_.data()),
-                        sizeof(char) * hostname_len_, 0);
-          } break;
-          case MsgType::kList: {
-            std::lock_guard<std::mutex> lock(clients_mutex_);
-            const size_t list_len = clients_.size();
+            SendMessage(fd, reinterpret_cast<void *>(&cur_time),
+                        sizeof(cur_time), 0);
+          }
+        }
+        break;
+        case MsgType::kHostname:
+        {
+          std::lock_guard<std::mutex> lock(mutex);
+          SendMessage(fd, reinterpret_cast<void *>(&msg_type),
+                      sizeof(msg_type), 0);
+          SendMessage(fd, reinterpret_cast<void *>(&hostname_len_),
+                      sizeof(hostname_len_), 0);
+          SendMessage(fd, reinterpret_cast<void *>(hostname_.data()),
+                      sizeof(char) * hostname_len_, 0);
+        }
+        break;
+        case MsgType::kList:
+        {
+          std::lock_guard<std::mutex> lock(clients_mutex_);
+          const size_t list_len = clients_.size();
+          {
+            std::lock_guard<std::mutex> lock(mutex);
+            SendMessage(fd, reinterpret_cast<void *>(&msg_type),
+                        sizeof(msg_type), 0);
+            SendMessage(fd, reinterpret_cast<const void *>(&list_len),
+                        sizeof(list_len), 0);
+            for (const auto &client : clients_)
+            {
+              SendMessage(fd, reinterpret_cast<const void *>(&client.first),
+                          sizeof(client.first), 0);
+              SendMessage(fd, reinterpret_cast<const void *>(&client.second),
+                          sizeof(client.second), 0);
+            }
+          }
+        }
+        break;
+        case MsgType::kMsg:
+        {
+          int dst;
+          size_t msg_len;
+          std::string msg;
+          RecvMessage(fd, reinterpret_cast<void *>(&dst), sizeof(dst), 0);
+          RecvMessage(fd, reinterpret_cast<void *>(&msg_len), sizeof(msg_len),
+                      0);
+          msg.resize(msg_len);
+          RecvMessage(fd, reinterpret_cast<void *>(msg.data()),
+                      sizeof(char) * msg_len, 0);
+          std::cout << "Sent Message to " << dst << " from " << fd << ": "
+                    << msg << std::endl;
+          if (msg_queues_.count(dst))
+          {
+            msg_queues_[dst].push(MessageInfo{fd, msg});
+            msg_type = static_cast<unsigned>(MsgType::kSuccess);
             {
               std::lock_guard<std::mutex> lock(mutex);
               SendMessage(fd, reinterpret_cast<void *>(&msg_type),
                           sizeof(msg_type), 0);
-              SendMessage(fd, reinterpret_cast<const void *>(&list_len),
-                          sizeof(list_len), 0);
-              for (const auto &client : clients_) {
-                SendMessage(fd, reinterpret_cast<const void *>(&client.first),
-                            sizeof(client.first), 0);
-                SendMessage(fd, reinterpret_cast<const void *>(&client.second),
-                            sizeof(client.second), 0);
-              }
             }
-          } break;
-          case MsgType::kMsg: {
-            int dst;
-            size_t msg_len;
-            std::string msg;
-            RecvMessage(fd, reinterpret_cast<void *>(&dst), sizeof(dst), 0);
-            RecvMessage(fd, reinterpret_cast<void *>(&msg_len), sizeof(msg_len),
-                        0);
-            msg.resize(msg_len);
-            RecvMessage(fd, reinterpret_cast<void *>(msg.data()),
-                        sizeof(char) * msg_len, 0);
-            std::cout << "Sent Message to " << dst << " from " << fd << ": "
-                      << msg << std::endl;
-            if (msg_queues_.count(dst)) {
-              msg_queues_[dst].push(MessageInfo{fd, msg});
-              msg_type = static_cast<unsigned>(MsgType::kSuccess);
-              {
-                std::lock_guard<std::mutex> lock(mutex);
-                SendMessage(fd, reinterpret_cast<void *>(&msg_type),
-                            sizeof(msg_type), 0);
-              }
-            } else {
-              msg_type = static_cast<unsigned>(MsgType::kError);
-              {
-                std::lock_guard<std::mutex> lock(mutex);
-                SendMessage(fd, reinterpret_cast<void *>(&msg_type),
-                            sizeof(msg_type), 0);
-              }
-            }
-          } break;
-          case MsgType::kHeartBeat: {
-            recv_heartbeat = true;
-          } break;
-          default:
-            std::cout << "unknown message type: " << msg_type << std::endl;
-          case MsgType::kDisconnect: {
-            close(fd);
-            std::lock_guard<std::mutex> lock(clients_mutex_);
-            clients_.erase(fd);
-            char address[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &addr.sin_addr, address, sizeof(address));
-            std::cout << address << ':' << addr.sin_port << " disconnected."
-                      << std::endl;
-            return;
           }
+          else
+          {
+            msg_type = static_cast<unsigned>(MsgType::kError);
+            {
+              std::lock_guard<std::mutex> lock(mutex);
+              SendMessage(fd, reinterpret_cast<void *>(&msg_type),
+                          sizeof(msg_type), 0);
+            }
+          }
+        }
+        break;
+        case MsgType::kHeartBeat:
+        {
+          recv_heartbeat = true;
+        }
+        break;
+        default:
+          std::cout << "unknown message type: " << msg_type << std::endl;
+        case MsgType::kDisconnect:
+        {
+          close(fd);
+          std::lock_guard<std::mutex> lock(clients_mutex_);
+          clients_.erase(fd);
+          char address[INET_ADDRSTRLEN];
+          inet_ntop(AF_INET, &addr.sin_addr, address, sizeof(address));
+          std::cout << address << ':' << addr.sin_port << " disconnected."
+                    << std::endl;
+          return;
+        }
         }
       }
       // check if alive
-      if (alive.wait_for(std::chrono::milliseconds(1)) ==
-          std::future_status::ready) {
-        close(fd);
-        std::lock_guard<std::mutex> lock(clients_mutex_);
-        clients_.erase(fd);
-        char address[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &addr.sin_addr, address, sizeof(address));
-        std::cout << address << ':' << addr.sin_port
-                  << " disconnected accidentally." << std::endl;
-        return;
-      }
+      // if (alive.wait_for(std::chrono::milliseconds(1)) ==
+      //     std::future_status::ready) {
+      //   close(fd);
+      //   std::lock_guard<std::mutex> lock(clients_mutex_);
+      //   clients_.erase(fd);
+      //   char address[INET_ADDRSTRLEN];
+      //   inet_ntop(AF_INET, &addr.sin_addr, address, sizeof(address));
+      //   std::cout << address << ':' << addr.sin_port
+      //             << " disconnected accidentally." << std::endl;
+      //   return;
+      // }
     }
   };
   threads_[fd] =
